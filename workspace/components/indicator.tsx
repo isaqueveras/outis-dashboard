65452a1d89d0ab0a7b0d952ca560e35f4c2bc7f6
Isaque Veras <isaqueveras@alu.ufc.br>
2024-06-02 13:20:55 -0300

    feat: indicator component

--- workspace/components/indicator.tsx
+++ workspace/components/indicator.tsx
@@ -1,18 +1,25 @@
-import { Box, Heading, Text } from "@chakra-ui/react"
+import { useFetch } from "@/app/hooks/useFetch"
+import { Box, Grid, GridItem, Heading, Text } from "@chakra-ui/react"
 
-type IndicatorProps = {
-  name: string
-  desc: string
-  value: any
-}
+const Indicator = () => {
+  const { data, error, isLoading, isValidating } = useFetch('/api/watcher/84cbfa32/routine/416e4b00-a8db-4b54-afbc-82de0016edf0/indicator')
+  if (error) return <div>falhou ao carregar</div>
+  if (isLoading) return <div>carregando</div>
 
-const Indicator = (indicator: IndicatorProps) => {
   return (
-    <Box>
-      <Heading fontSize={'54px'} mb={3}>{indicator.value}</Heading>
-      <Text>{indicator.name}</Text>
-      <Text textColor={'gray'}>{indicator.desc}</Text>
-    </Box>
+    <Grid templateColumns='repeat(5, 1fr)' gap={4}>
+      {data.data.map((value: any, i: any) => {
+        return (
+          <GridItem colSpan={1} bg='#1f1e1e' p={7} key={i}>
+            <Box>
+              <Heading fontSize={'54px'} mb={3}>{value?.value}</Heading>
+              <Text>{value?.name}</Text>
+              <Text textColor={'gray'}>{value?.desc}</Text>
+            </Box>
+          </GridItem>
+        )
+      })}
+    </Grid>
   )
 }
 
